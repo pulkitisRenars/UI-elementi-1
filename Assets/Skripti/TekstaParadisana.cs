3d47b47ea3a948a3a06c760095d1ea46150bd8c6
Kristaps <r.kristaps@inbox.lv>
2022-02-16 14:14:51 +0200

    Īstenotas darbīas ar tekstu

---
+++ Assets/Skripti/TekstaParadisana.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TekstaParadisana : MonoBehaviour {
+	//Uzglabās tekstu, kurš paņemts no input lauka
+	public string teksts;
+	//Lauks no, kura es tekstu paņemšu
+	public GameObject ievadesLauks;
+	//Teksta objekts, kur attēlot mainīga teksts saturu
+	public GameObject tekstaAttelosanasLauks;
+
+	public void darbibasArTekstu(){
+		teksts = ievadesLauks.GetComponent<Text> ().text;
+		tekstaAttelosanasLauks.GetComponent<Text>().text = "Sveiks "+teksts.ToUpper()+"!";
+	}
+}
