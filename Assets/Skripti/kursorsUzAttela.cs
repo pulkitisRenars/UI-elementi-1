73d08375dcc08fb9ce8cc96a4d53fcfb7986a20b
renars <renars.pulkis@gmail.com>
2022-02-22 15:28:24 +0200

    yesss

---
+++ Assets/Skripti/kursorsUzAttela.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kursorsUzAttela : MonoBehaviour {
+
+	public AudioSource skanasAvots;
+	public AudioClip skanaKoAtskanot;
+
+	public void uzbiditUzAttela(){
+		skanasAvots.PlayOneShot (skanaKoAtskanot);
+	}
+	public void nobiditnoAttela(){
+		skanasAvots.Stop ();
+	}
+}
