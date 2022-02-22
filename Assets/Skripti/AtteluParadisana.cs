b172194a65acc1de3d43b20540858efa4c7e8e68
renars <renars.pulkis@gmail.com>
2022-02-22 10:44:18 +0200

    DONEZO

--- Assets/Skripti/AtteluParadisana.cs
+++ Assets/Skripti/AtteluParadisana.cs
@@ -1,12 +1,16 @@
-﻿using System.Collections;
+﻿ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AtteluParadisana : MonoBehaviour {
 	public GameObject bins;
 	public GameObject lacitis;
 	public GameObject tante;
 	public GameObject masina;
+	public GameObject attelaTuretajs;
+	public Sprite[] atteluMasivs;
+	public GameObject slider;
 
 	public void binaAttelosana(bool vertiba){
 		bins.SetActive (vertiba);
@@ -30,5 +34,20 @@ public class AtteluParadisana : MonoBehaviour {
 
 	public void paLabiBins(){
 		bins.transform.localScale = new Vector2 (-1, 1);
+	}
+
+	public void izkritosais(int skaitlis){
+		if (skaitlis == 0)
+			attelaTuretajs.GetComponent<Image> ().sprite = atteluMasivs [0];
+		else if (skaitlis == 1)
+			attelaTuretajs.GetComponent<Image> ().sprite = atteluMasivs [1];
+		else if (skaitlis == 2)
+			attelaTuretajs.GetComponent<Image> ().sprite = atteluMasivs [2];
+	}
+
+	public void mainitLiel(){
+		float pasreizVal = slider.GetComponent<Slider> ().value;
+		attelaTuretajs.transform.localScale = new Vector2 (1F * pasreizVal, 1F * pasreizVal);
+
 	}
 }
